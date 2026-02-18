e66571fbd531acd7af2e63bdde195d4338f10333
CantUseNamePerson <xianyin2014@outlook.com>
2026-02-18 18:06:04 +0800

    implemented djb2 hash

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
-int main() {}
+int djb2(std::string &str) {
+  int hash = 5381;
+  for (char c : str) {
+    hash = (hash << 5) + hash + c;
+  }
+  return hash;
+}
+int main() {
+  std::string str;
+  std::cin >> str;
+  int hash = djb2(str);
+  std::cout << hash << '\n';
+}
