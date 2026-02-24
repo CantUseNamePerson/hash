72b53e61efb4e97d9aa5f161ce1bd02f76b16abd
CantUseNamePerson <xianyin2014@outlook.com>
2026-02-24 12:09:03 +0800

    did more random stuff

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <unordered_map>
 
+
 // djb2 hash, popular for strings
 unsigned long long djb2(std::string &str) {
   unsigned long long hash = 5381; // magic number
@@ -18,8 +19,7 @@ unsigned long long fnv1a(std::string &str) {
   // for every char in str
   for (char c : str) {
     hash ^= static_cast<unsigned long long>(
-        c); // add current bits from c (the char) and change the type to
-            // unsigned long long
+        c); // add current bits from c (the char) and change the type to unsigned long long
     hash *= 1099511628211ULL; // multiply by FNV_prime
   }
   return hash;
@@ -42,21 +42,24 @@ void test() {
   std::unordered_map<unsigned long long, std::string> seen;
   for (char a = '0'; a <= 'z'; a++) {
     for (char b = '0'; b <= 'z'; b++) {
-      std::string s = {a, b};
-      unsigned long long hash = custom(s);
-      if (seen.count(hash)) {
-        collision++;
-        std::cout << "Collision: " << s << " and " << seen[hash]
-                  << " both hash to " << hash << "\n";
-      } else {
-        seen[hash] = s;
-        // std::cout << "No collision, for: " << s << ", hash is: " << hash <<
-        // '\n';
+      for (char c = '0'; c <= 'z'; c++) {
+        std::string s = {a, b, c};
+        unsigned long long hash = custom(s);
+        if (seen.count(hash)) {
+          collision++;
+          std::cout << "Collision: " << s << " and " << seen[hash] << " both hash to " << hash << "\n";
+        } else {
+          seen[hash] = s;
+          // std::cout << "No collision, for: " << s << ", hash is: " << hash << '\n';
+        }
       }
     }
   }
+  std::cout << "Total collisions: " << collision << '\n';
   return;
 }
 
 // main
-int main() { test(); }
+int main() {
+    test();
+}
